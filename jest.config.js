df5cba99afcce269c04f1d15b28014444efeb37e
yenshih <yenshih95@gmail.com>
2018-06-22 11:50:48 +0800

    Update lint config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
 module.exports = {
     transform: {
-        '.(ts|tsx)': './node_modules/ts-jest/preprocessor.js'
+        '.(ts|tsx)': './node_modules/ts-jest/preprocessor.js',
     },
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
     moduleFileExtensions: [
         'ts',
         'tsx',
-        'js'
+        'js',
     ],
     coveragePathIgnorePatterns: [
         'node_modules',
-        'test'
+        'test',
     ],
     testURL: 'https://localhost',
     silent: true,
